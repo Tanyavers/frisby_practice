93225e1667b358a227703e9220f449f910bf4f4d
Tanyavers <vershinina572@gmail.com>
2023-07-06 12:33:18 +0200

    skip test day16

--- __tests__/Day16.js
+++ __tests__/Day16.js
@@ -1,16 +1,14 @@
 const frisby = require("frisby");
 
-jest.setTimeout(20_000);
-
-it("Listing web pages", async () => {
+it.skip("Listing web pages", async () => {
     let page = 0;
     let status = 200;
 
     while (status == 200) {
       page++;
-      const response = await frisby.get(`http://xkcd.com/${page}/info.0.json`);
+      const response = await frisby.get(`http://xkcd.com/${page}/info.0.json`, {timeout: 15_000});
       status = response.status;
     }
     console.log(page);
     expect(page).toEqual(404);
-});
+}, 30_000);
